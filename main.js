524174e95a7d1a0acad59ffa66ee06f471b136a3
pedrotanak <106190056+pedrotanak@users.noreply.github.com>
2022-09-13 17:14:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-var Guestname=document.getElementById("name1").value;
-namesOfPeople.push(Guestname);
-namesOfPeople.sort();
-function searching()
-{
-    var s=document.getElementById(!"s1").value;
-    var found=0;
-    var j;
-    for(j=0; j<namesOfPeople.length; j++){
-        if(s==namesOfPeople; [j]){
-            found=found+1;
-        }
-    }
-document.getElementById("p2").innerHTML="Nome encontrado"+found+"vez(es)";
-console.log("Nome encontrado" +found+"vez(es)");
+var Guestname= "";
+var namesOfPeople=[];
+
+
+function enviar(){
+    Guestname=document.getElementById("name1").value;
+  namesOfPeople.push(Guestname);
+   
+  console.log(namesOfPeople);
+  document.getElementById("lista").innerHTML=namesOfPeople;  
 }
+function organizar (){
+  namesOfPeople.sort();  
+  document.getElementById("lista").innerHTML=namesOfPeople;
+}
+
+
+// function searching()
+// {
+//     var s=document.getElementById(!"s1").value;
+//     var found=0;
+//     var j;
+//     for(j=0; j<namesOfPeople.length; j++){
+//         if(s==namesOfPeople;[j])
+//         {
+//             found=found+1;}
+//     }
+// document.getElementById("p2").innerHTML="Nome encontrado"+found+"vez(es)";
+// console.log("Nome encontrado" +found+"vez(es)");
+// }
